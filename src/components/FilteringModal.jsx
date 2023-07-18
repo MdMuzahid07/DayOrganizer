c490e2cb5faa9b981fb15b0de414c25019743c68
Your Name <mdmuzahid7396@gmail.com>
2023-07-19 00:49:12 +0600

    Day Organizer, UI completed

--- src/components/FilteringModal.jsx
+++ src/components/FilteringModal.jsx
@@ -26,11 +26,20 @@ const FilteringModal = () => {
                         <button className="px-3 border border-4 border-blue-300 text-xs rounded-full text-white font-bold" style={{ background: "#8CC0DE" }}>Find</button>
                     </form>
 
-                    <div className="flex items-center gap-2 my-4">
+                    <div className="flex items-center gap-2 my-5">
                         <button className="text-xs text-white border border-2 border-white font-bold px-3 rounded-full" style={{ background: "#CCEEBC" }}>All</button>
                         <button className="text-xs text-white border border-2 border-white font-bold px-3 rounded-full" style={{ background: "#FFD9C0" }}>Incomplete</button>
                         <button className="text-xs text-lime-400 border border-2 border-white font-bold px-3 rounded-full" style={{ background: "#FAF0D7" }}>Complete</button>
                     </div>
+
+                    <hr className="border-t-2 border-white mb-4" />
+
+                    <div className="flex flex-wrap items-center gap-2">
+                        <button className="text-xs text-white font-bold px-3 border border-2 border-white rounded-full bg-green-400">Low Priority</button>
+                        <button className="text-xs text-white font-bold px-3 border border-2 border-white rounded-full bg-yellow-400">Medium Priority</button>
+                        <button className="text-xs text-white font-bold px-3 border border-2 border-white rounded-full bg-red-400">Most Priority</button>
+                    </div>
+
                 </div>
             }
         </section>
